cf2be452a57a587ecefbe3f15135377f71bfde6a
Sangwoo Park <sangwopark19@gmail.com>
2023-12-19 20:41:10 +0900

    refactor: Address 클래스 Builder 적용

--- backend/src/main/java/io/toytech/backend/member/domain/Address.java
+++ backend/src/main/java/io/toytech/backend/member/domain/Address.java
@@ -1,27 +1,37 @@
 package io.toytech.backend.member.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
-
-  /**
-   * 상세주소 (예: 신흥로 000-0)
-   */
-  private String street;
-
-  /**
-   * 도시 (예: 부천시)
-   */
-  private String city;
 
   /**
    * 도 (예: 경기도)
    */
   private String state;
-
+  /**
+   * 도시 (예: 부천시)
+   */
+  private String city;
+  /**
+   * 상세주소 (예: 신흥로 000-0)
+   */
+  private String street;
   /**
    * 우편번호
    */
   private String zipCode;
+
+
+  @Builder
+  public Address(String state, String city, String street, String zipCode) {
+    this.state = state;
+    this.city = city;
+    this.street = street;
+    this.zipCode = zipCode;
+  }
 }
