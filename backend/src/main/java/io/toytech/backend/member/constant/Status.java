bf12421e2c1691cc306aafeb568936deeca58143
Sangwoo Park <sangwopark19@gmail.com>
2023-12-23 21:22:14 +0900

    chore: Status 열거형 주석 추가

--- backend/src/main/java/io/toytech/backend/member/constant/Status.java
+++ backend/src/main/java/io/toytech/backend/member/constant/Status.java
@@ -4,12 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum Status {
-  ADMIN("A"),
-  MEMBER("M"),
-  GUEST("G"),
-  BANNED("B"),
-  DELETED("D"),
-  INACTIVE("I");
+  ADMIN("A"), // 관리자
+  MEMBER("M"), // 일반 회원
+  GUEST("G"), // 게스트
+  BANNED("B"), // 정지된 회원
+  DELETED("D"), // 삭제된 회원
+  INACTIVE("I"); // 휴면 회원
 
   private final String code;
 
