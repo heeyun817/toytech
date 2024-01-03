93936bac83ce2811ebeeda0db671b21e8fd0750b
Sangwoo Park <sangwopark19@gmail.com>
2024-01-03 17:17:54 +0900

    feat: MemberCreateRequest email 유효성 검증기능 구현

--- backend/src/main/java/io/toytech/backend/domain/member/dto/MemberCreateRequest.java
+++ backend/src/main/java/io/toytech/backend/domain/member/dto/MemberCreateRequest.java
@@ -2,8 +2,10 @@ package io.toytech.backend.domain.member.dto;
 
 import io.toytech.backend.domain.member.constant.Status;
 import io.toytech.backend.domain.member.domain.Member;
+import jakarta.validation.constraints.Email;
 
 public record MemberCreateRequest(
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     String email,
     String name,
     String password
